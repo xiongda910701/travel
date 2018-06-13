604485e01fb34b9d03ab6c98f1efdd09219e1ea2
xiongda <962008509@qq.com>
2018-06-13 16:04:08 +0800

    project reset

---
+++ src/containers/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home-page">
+        <div>{{ msg }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {
+                msg: 'home page'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
